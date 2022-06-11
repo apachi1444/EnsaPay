8eaddbcc63a31c13cf08477e6fd353581d922c9d
unknown <yessinejawa@gmail.com>
2022-06-11 13:33:30 +0200

    adding the phone and the solde in the client form

--- src/app/agent-module/agent-components/add-client/add-client.component.ts
+++ src/app/agent-module/agent-components/add-client/add-client.component.ts
@@ -5,46 +5,44 @@ import { AgentServiceService } from '../../agent-services/agent-service.service'
 @Component({
   selector: 'app-add-client',
   templateUrl: './add-client.component.html',
-  styleUrls: ['./add-client.component.css']
+  styleUrls: ['./add-client.component.css'],
 })
 export class AddClientComponent implements OnInit {
-  Image:any
-  constructor(private agentService:AgentServiceService) { }
-   src:any
-   nameImage:any="choose file"
-  ngOnInit(): void {
+  Image: any;
+  constructor(private agentService: AgentServiceService) {}
+  src: any;
+  nameImage: any = 'choose file';
+  ngOnInit(): void {}
+  File(event: any) {
+    let file = event.target.files[0];
+    console.log(file);
+    this.valider(file);
   }
-  File(event:any){
-    let file=event.target.files[0]
-    console.log(file)
-    this.valider(file)
+  valider(file: any) {
+    let validFile = new RegExp('(png)|(jpg)|(jpeg)$');
+    if (validFile.test(file.type)) {
+      this.Image = file;
+      let newFileReader = new FileReader();
+      newFileReader.onload = () => {
+        let imageUrl = newFileReader.result;
+        this.src = newFileReader.result;
+        this.nameImage = file.name;
+      };
+      newFileReader.readAsDataURL(file);
+    } else {
+      alert('image must be jpg or png or jpeg');
+    }
   }
-  valider(file:any){
-    let validFile = new RegExp("(png)|(jpg)|(jpeg)$");
-    if(validFile.test(file.type)){
-      this.Image=file
-      let newFileReader=new FileReader();
-      newFileReader.onload=(()=>{
-        let imageUrl=newFileReader.result;
-        this.src=newFileReader.result;
-        this.nameImage=file.name
-      });
-      newFileReader.readAsDataURL(file)
-    }
-    else{
-      alert("image must be jpg or png or jpeg" )
-    }
 
+  addClient(event: any) {
+    console.log(event.currentTarget);
+    const myForm = event.currentTarget;
+    const data = new FormData(myForm);
+    data.append('file', this.Image);
+    this.agentService.postClient(data);
   }
-  
-addClient(event:any){
-const myForm = event.currentTarget;
-const data = new FormData(myForm);
-data.append("file",this.Image)
-this.agentService.postClient(data)
-}
-suprrimerPhoto(){
-  this.src=null;
-  this.nameImage="choose file"
-}
+  suprrimerPhoto() {
+    this.src = null;
+    this.nameImage = 'choose file';
+  }
 }
